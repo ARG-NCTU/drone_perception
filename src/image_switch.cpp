6b1f8d03dda095393c1ab5157ae44cab6bd77f02
bawkbak <hsiehbawkbak@gmail.com>
2022-11-10 10:38:10 +0800

    Fix: Update missing code

--- src/image_switch.cpp
+++ src/image_switch.cpp
@@ -2,6 +2,7 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <image_transport/image_transport.h>
 #include <behavior_tree/behavior_tree.h>
 
@@ -14,11 +15,13 @@ string appendString(const string &s_body, const string &s_suffix){
     return origin;
 }
 
+// d435_backward_color_optical_frame
+// zedm_forward_left_camera_optical_frame
+// d435_downward_color_optical_frame
 class Switcher{
     private:
-        
-
-        int m_current_camera_index = 0; // 1 = forward, 2 = downward, 3 = backward
+        // 1 = forward, 2 = downward, 3 = backward
+        int m_current_camera_index = 0; 
     public:
         bt::Action switch_forward;
         bt::Action switch_downward;
@@ -26,24 +29,54 @@ class Switcher{
 
         ros::NodeHandle nh;
         image_transport::ImageTransport it;
-        image_transport::Subscriber sub_camera_forward_img;
-        image_transport::Subscriber sub_camera_downward_img;
-        image_transport::Subscriber sub_camera_backward_img;
-        image_transport::Publisher pub_img;
+        image_transport::Subscriber sub_camera_forward_img_rgb;
+        image_transport::Subscriber sub_camera_downward_img_rgb;
+        image_transport::Subscriber sub_camera_backward_img_rgb;
 
+        image_transport::Subscriber sub_camera_forward_img_depth;
+        image_transport::Subscriber sub_camera_downward_img_depth;
+        image_transport::Subscriber sub_camera_backward_img_depth;
+
+        image_transport::Publisher pub_img_rgb;
+        image_transport::Publisher pub_img_depth;
+
+        ros::Subscriber sub_camera_forward_info;
+        ros::Subscriber sub_camera_downward_info;
+        ros::Subscriber sub_camera_backward_info;
+        ros::Publisher pub_camera_info;
         
 
-        sensor_msgs::Image msg_camera_forward;
-        sensor_msgs::Image msg_camera_downward;
-        sensor_msgs::Image msg_camera_backward;
+        sensor_msgs::Image msg_camera_forward_rgb;
+        sensor_msgs::Image msg_camera_downward_rgb;
+        sensor_msgs::Image msg_camera_backward_rgb;
+
+        sensor_msgs::Image msg_camera_forward_depth;
+        sensor_msgs::Image msg_camera_downward_depth;
+        sensor_msgs::Image msg_camera_backward_depth;
+
+        sensor_msgs::CameraInfo msg_camera_forward_info;
+        sensor_msgs::CameraInfo msg_camera_downward_info;
+        sensor_msgs::CameraInfo msg_camera_backward_info;
+        
         
         Switcher();
-        void imageForwardCallback(const sensor_msgs::Image::ConstPtr& msg);
-        void imageDownwardCallback(const sensor_msgs::Image::ConstPtr& msg);
-        void imageBackwardCallback(const sensor_msgs::Image::ConstPtr& msg);
+        void imageRGBForwardCallback(const sensor_msgs::Image::ConstPtr& msg);
+        void imageRGBDownwardCallback(const sensor_msgs::Image::ConstPtr& msg);
+        void imageRGBBackwardCallback(const sensor_msgs::Image::ConstPtr& msg);
+
+        void imageDepthForwardCallback(const sensor_msgs::Image::ConstPtr& msg);
+        void imageDepthDownwardCallback(const sensor_msgs::Image::ConstPtr& msg);
+        void imageDepthBackwardCallback(const sensor_msgs::Image::ConstPtr& msg);
+
+        void camInfoForwardCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
+        void camInfoDownwardCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
+        void camInfoBackwardCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
+
         void actionSet(int action_index, int state);
         void switchImageSource(int camera_index);
-        void publishImage(int camera_index);
+        void publishRGB();
+        void publishDepth();
+        void publishInfo();
         void visualize();
 };
 
@@ -51,48 +84,128 @@ Switcher :: Switcher() : switch_forward(appendString(ros::this_node::getName(), 
                         switch_downward(appendString(ros::this_node::getName(), (string)"_downward")),
                         switch_backward(appendString(ros::this_node::getName(), (string)"_backward")),
                         it(nh){
-    sub_camera_forward_img = it.subscribe("forward/image", 1,  &Switcher::imageForwardCallback, this);
-    sub_camera_downward_img = it.subscribe("downward/image", 1,  &Switcher::imageDownwardCallback, this);
-    sub_camera_backward_img = it.subscribe("backward/image", 1,  &Switcher::imageBackwardCallback, this);
-    pub_img = it.advertise("desired_image", 10);
-}
+    sub_camera_forward_img_rgb = it.subscribe("forward/image/rgb", 1,  &Switcher::imageRGBForwardCallback, this);
+    sub_camera_downward_img_rgb = it.subscribe("downward/image/rgb", 1,  &Switcher::imageRGBDownwardCallback, this);
+    sub_camera_backward_img_rgb = it.subscribe("backward/image/rgb", 1,  &Switcher::imageRGBBackwardCallback, this);
+    pub_img_rgb = it.advertise("desired_image/rgb", 10);
 
-void Switcher :: imageForwardCallback(const sensor_msgs::Image::ConstPtr& msg){
-    msg_camera_forward = *msg;
-    publishImage(1);
+    sub_camera_forward_img_depth = it.subscribe("forward/image/depth", 1,  &Switcher::imageDepthForwardCallback, this);
+    sub_camera_downward_img_depth = it.subscribe("downward/image/depth", 1,  &Switcher::imageDepthDownwardCallback, this);
+    sub_camera_backward_img_depth = it.subscribe("backward/image/depth", 1,  &Switcher::imageDepthBackwardCallback, this);
+    pub_img_depth = it.advertise("desired_image/depth", 10);
+
+    sub_camera_forward_info = nh.subscribe<sensor_msgs::CameraInfo>("forward/image/info", 1,  &Switcher::camInfoForwardCallback, this);
+    sub_camera_downward_info = nh.subscribe<sensor_msgs::CameraInfo>("downward/image/info", 1,  &Switcher::camInfoDownwardCallback, this);
+    sub_camera_backward_info = nh.subscribe<sensor_msgs::CameraInfo>("backward/image/info", 1,  &Switcher::camInfoBackwardCallback, this);
+    pub_camera_info = nh.advertise<sensor_msgs::CameraInfo>("desired_image/info", 10);
+    
+}
+// rgb
+void Switcher :: imageRGBForwardCallback(const sensor_msgs::Image::ConstPtr& msg){
+    msg_camera_forward_rgb = *msg;
+    publishRGB();
     return;
 }
 
-void Switcher :: imageDownwardCallback(const sensor_msgs::Image::ConstPtr& msg){
-    msg_camera_downward = *msg;
-    publishImage(2);
+void Switcher :: imageRGBDownwardCallback(const sensor_msgs::Image::ConstPtr& msg){
+    msg_camera_downward_rgb = *msg;
+    publishRGB();
     return;
 }
 
-void Switcher :: imageBackwardCallback(const sensor_msgs::Image::ConstPtr& msg){
-    msg_camera_backward = *msg;
-    publishImage(3);
+void Switcher :: imageRGBBackwardCallback(const sensor_msgs::Image::ConstPtr& msg){
+    msg_camera_backward_rgb = *msg;
+    publishRGB();
+    return;
+}
+// depth
+void Switcher :: imageDepthForwardCallback(const sensor_msgs::Image::ConstPtr& msg){
+    msg_camera_forward_depth = *msg;
+    publishDepth();
     return;
 }
 
-void Switcher :: publishImage(int camera_index){
-    // if(camera_index != m_current_camera_index){ return; }
-    // else{
-        switch(m_current_camera_index){
-            case 1:
-                pub_img.publish(msg_camera_forward);
-                ROS_INFO("Forward");
-                break;
-            case 2:
-                pub_img.publish(msg_camera_downward);
-                ROS_INFO("Downward");
-                break;
-            case 3:
-                pub_img.publish(msg_camera_backward);
-                ROS_INFO("Backward");
-                break;
-        }
-    // }
+void Switcher :: imageDepthDownwardCallback(const sensor_msgs::Image::ConstPtr& msg){
+    msg_camera_downward_depth = *msg;
+    publishDepth();
+    return;
+}
+
+void Switcher :: imageDepthBackwardCallback(const sensor_msgs::Image::ConstPtr& msg){
+    msg_camera_backward_depth = *msg;
+    publishDepth();
+    return;
+}
+// cam_info
+void Switcher :: camInfoForwardCallback(const sensor_msgs::CameraInfo::ConstPtr& msg){
+    msg_camera_forward_info = *msg;
+    publishInfo();
+    return;
+}
+
+void Switcher :: camInfoDownwardCallback(const sensor_msgs::CameraInfo::ConstPtr& msg){
+    msg_camera_downward_info = *msg;
+    publishInfo();
+    return;
+}
+
+void Switcher :: camInfoBackwardCallback(const sensor_msgs::CameraInfo::ConstPtr& msg){
+    msg_camera_backward_info = *msg;
+    publishInfo();
+    return;
+}
+
+void Switcher :: publishRGB(){
+    switch(m_current_camera_index){
+        case 1:
+            pub_img_rgb.publish(msg_camera_forward_rgb);
+            // ROS_INFO("Forward");
+            break;
+        case 2:
+            pub_img_rgb.publish(msg_camera_downward_rgb);
+            // ROS_INFO("Downward");
+            break;
+        case 3:
+            pub_img_rgb.publish(msg_camera_backward_rgb);
+            // ROS_INFO("Backward");
+            break;
+    }
+    return;
+}
+
+void Switcher :: publishDepth(){
+    switch(m_current_camera_index){
+        case 1:
+            pub_img_depth.publish(msg_camera_forward_depth);
+            // ROS_INFO("Forward");
+            break;
+        case 2:
+            pub_img_depth.publish(msg_camera_downward_depth);
+            // ROS_INFO("Downward");
+            break;
+        case 3:
+            pub_img_depth.publish(msg_camera_backward_depth);
+            // ROS_INFO("Backward");
+            break;
+    }
+    return;
+}
+
+void Switcher :: publishInfo(){
+    switch(m_current_camera_index){
+        case 1:
+            pub_camera_info.publish(msg_camera_forward_info);
+            // ROS_INFO("Forward");
+            break;
+        case 2:
+            pub_camera_info.publish(msg_camera_downward_info);
+            // ROS_INFO("Downward");
+            break;
+        case 3:
+            pub_camera_info.publish(msg_camera_backward_info);
+            // ROS_INFO("Backward");
+            break;
+    }
     return;
 }
 
@@ -152,7 +265,7 @@ void Switcher :: switchImageSource(int camera_index){
 void Switcher :: visualize(){
     ros::Rate rate(30);
     // ROS_INFO("Now: %d", m_current_camera_index);
-    // publishImage(3);
+    // publishRGB(3);
     rate.sleep();
     return;
 }
